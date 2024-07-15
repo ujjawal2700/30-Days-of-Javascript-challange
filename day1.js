03460e14cfffc13d6a3cf7e2aa7e65b3f6f2f07f
Ujjawal Mahawar <ujjawalmahawar@Ujjawals-MacBook-Air.local>
2024-07-15 17:17:02 +0530

    first commit

---
+++ day1.js
@@ -0,0 +1,56 @@
+// Activity 1 - Variable Declaration
+
+// Task 1 : Declare the variable using var, assign it a number and log the value to the console.
+
+/* var num = 10;
+
+console.log(num); */
+
+// Task 2 : Declare the variable using let, assign it a string and log the value to the console.
+
+/* let num1 = 20;
+
+console.log(num1); */
+
+// Activity 2 - Constant Declaration
+
+// Task 3 : Declare a variable using const, assign it a boolean value and log the value to the console.
+
+/* const num2 = true;
+
+console.log(num2); */
+
+// Activity 3 - Data types
+
+// Task 4 : Create variables of different data types (number, string, boolean, object array) and log each variables type using the typeof operator.
+
+/* var number = 12;
+var str = "Ujjawal";
+var bool = false;
+var obj = {
+    name: "Ujjawal",
+    age: 22
+};
+const arr = ["Saab", "Volvo", "BMW"];
+
+console.log(typeof(number))
+console.log(typeof(str))
+console.log(typeof(bool))
+console.log(typeof(obj))
+console.log(typeof(arr)) */
+
+// Activity 5 - Reassigning variables
+
+// Task 4 : Declare a variable using let, assign it an inital value, reassign a new value, and log both values to the console.
+
+/* let num3 = 30;
+console.log(num3)
+num3 = 40;
+console.log(num3) */
+
+// Activity 6 - Understanding const 
+
+// Task 5 : Try reassigning a variable declared with const and observe the error.
+
+const num4 = 3;
+num4 = 5 //throws error
